2edcae6684c5fd6fd8e7e85fb1a6642b68c332d3
Yusuff Atanda <Yvsvfff@gmail.com>
2023-10-16 21:34:18 -0400

    update

---
+++ web-scrapper/app/page.tsx
@@ -0,0 +1,13 @@
+import { DocumentMagnifyingGlassIcon } from "@heroicons/react/24/outline"
+export default function HomePage() {
+  return (
+       <div>
+        <div className="flex flex-col items-center justify-center">
+          <DocumentMagnifyingGlassIcon  className="h-64 w-64 text-indigo-600/20"/>
+          <h1 className="text-3xl text-center text-black font-bold mb-5"> Welcome to Amazon Web Scrapper</h1>
+          <h2 className="text-lg italic text-center text-black/50">JB Financial Solutions</h2>
+          <h3 className="text-lg italic text-black/50 text-center">www.blog-yvsvff.vercel.app</h3>
+        </div>
+       </div>
+  )
+}
